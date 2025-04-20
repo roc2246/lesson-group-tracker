1497d3a62438bbeb0d5e910926473f77725e1a47
Riley <riley.childs@yahoo.com>
2025-04-20 13:41:36 -0400

    Got workstation ready for creating schema notes and retreiveLessons() test

--- controllers/api.test.js
+++ controllers/api.test.js
@@ -1,6 +1,21 @@
 import { describe, it } from "vitest";
 import { retrieveLessons } from "./api";
 
+/* 
+    query = `
+        SELECT * FROM lessons
+        WHERE instructor_id = ${instructorID} AND lesson_date = ?
+      `;
+    query = `
+        SELECT lesson_date, age_group, COUNT(*) AS total_lessons
+        FROM lessons
+        WHERE instructor_id = ${instructorID}
+        GROUP BY lesson_date, age_group
+        ORDER BY lesson_date ASC
+      `;
+    */
+
+
 it("should be test", async()=>{
     await retrieveLessons()
 })
