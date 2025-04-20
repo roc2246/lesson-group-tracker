e35a3c09b25ceb5220f582fc4ebac3b7fde6f050
Riley <riley.childs@yahoo.com>
2025-04-20 14:49:05 -0400

    Created sql for sorting guests of a day from age group baed on ability level

--- controllers/api.test.js
+++ controllers/api.test.js
@@ -2,6 +2,7 @@ import { describe, it } from "vitest";
 import { retrieveLessons } from "./api";
 
 /* 
+retrieveLessons()
     query = `
         SELECT * FROM lessons
         WHERE instructor_id = ${instructorID} AND lesson_date = ?
@@ -15,7 +16,18 @@ import { retrieveLessons } from "./api";
       `;
     */
 
+/* 
+      todaysStudents()
+      SELECT g.guest_id, g.name AS guest_name, g.level AS ability_level, g.birthdate AS guest_birthdate,
+       p.age_group, i.name AS instructor_name, p.lesson_date
+FROM PROGRAM p
+JOIN GUESTS g ON p.guest_id = g.guest_id
+JOIN INSTRUCTORS i ON p.instructor_id = i.instructor_id
+WHERE p.age_group = ?  -- Replace '?' with the desired age group
+ORDER BY g.level ASC;  -- Sort by ability level
 
-it("should be test", async()=>{
-    await retrieveLessons()
-})
+      */
+
+it("should be test", async () => {
+  await retrieveLessons();
+});
