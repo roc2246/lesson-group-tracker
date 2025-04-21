614b7c61aa5ef9f3bf1d289f9c6864560754db76
Riley <riley.childs@yahoo.com>
2025-04-21 09:25:44 -0400

    Refactored both api controllers to one function

--- controllers/api.js
+++ controllers/api.js
@@ -2,21 +2,21 @@ const db = require("../db");
 const utilities = require('../utilities')
 
 /* 
-NAME: retrieve lessons
+NAME: retrieveQuery
 INPUT: req, res, sql, connection
 */
 
-async function retrieveLessons(req, res, sql) {
+
+async function retrieveQuery(req, res, sql) {
   try {
     const connection = await db.dbLogin();
-    const instructorID = req.body.instructorID;
-    const lessonDate = req.body.lesson_date;
+    const inputs = req.body
 
-    utilities.validateInput(instructorID,  "Instructor ID")
+    for(const dataName in inputs){
+      utilities.validateInput(inputs[dataName],  dataName.toString())
+    }
 
-    const values = sql.includes("lesson_date")
-      ? [instructorID, lessonDate]
-      : [instructorID];
+    const values = Object.values(inputs);
 
     const results = await db.handleQuery(sql, values, connection);
     connection.end()
@@ -26,39 +26,6 @@ async function retrieveLessons(req, res, sql) {
   }
 }
 
-/* 
-NAME: todays students
-INPUT: req, res, conn
-*/
-
-// async function todaysStudents(req, res, conn = db.dbLogin) {
-//   try {
-//     const connection = await conn();
-//     const lessonDate = req.body.lesson_date;
-//     const ageGroup = req.body.age_group;
-
-//     // SQL query for age group and lesson date
-//     const sql = `
-//       SELECT g.guest_id, g.name AS guest_name, g.level AS ability_level, g.birthdate AS guest_birthdate,
-//              p.age_group, i.name AS instructor_name, p.lesson_date
-//       FROM PROGRAM p
-//       JOIN GUESTS g ON p.guest_id = g.guest_id
-//       JOIN INSTRUCTORS i ON p.instructor_id = i.instructor_id
-//       WHERE p.age_group = ? AND p.lesson_date = ?
-//       ORDER BY g.level ASC;
-//     `;
-//     const values = [ageGroup, lessonDate];
-
-//     const results = await db.handleQuery(sql, values, connection);
-//     connection.end(); // Close the connection
-//     return res.status(200).json({ lessons: results });
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({ error: "Failed to retrieve students for today." });
-//   }
-// }
-
 module.exports = {
-  retrieveLessons,
-  todaysStudents,
+  retrieveQuery
 };
