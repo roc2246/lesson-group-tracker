5d0556fcda8afaad24839e09601e7bf560cf865c
Riley <riley.childs@yahoo.com>
2025-04-21 09:51:20 -0400

    Removed notes from api.js

--- routers/api.js
+++ routers/api.js
@@ -1,22 +1,7 @@
 const router = require('express')
 const controllers = require('../controllers/api.js')
 
-// FOR ROUTERS INVOLVING LESSON RETREIVAL
-/* 
-    query = `
-        SELECT * FROM lessons
-        WHERE instructor_id = ${instructorID} AND lesson_date = ?
-      `;
-    query = `
-        SELECT lesson_date, age_group, COUNT(*) AS total_lessons
-        FROM lessons
-        WHERE instructor_id = ${instructorID}
-        GROUP BY lesson_date, age_group
-        ORDER BY lesson_date ASC
-      `;
-    */
 
-// Retreive all lessons, organize by day, and get just lessons by age group
 router.get("/all-lessons-group-name", (req, res)=>{
   const sql = `
         SELECT lesson_date, age_group, COUNT(*) AS total_lessons
@@ -27,7 +12,7 @@ router.get("/all-lessons-group-name", (req, res)=>{
       `
   controllers.retrieveQuery(req, res, sql)
 })
-// Retreive lesson for just one day and get just lessons by age group
+
 router.get('/lesson-groups-for-day', (req, res)=>{
   const sql = `
         SELECT * FROM lessons
@@ -35,7 +20,7 @@ router.get('/lesson-groups-for-day', (req, res)=>{
       `
   controllers.retrieveQuery(req, res, sql)
 })
-// Retrieve age group for one day and sort students by ability level
+
 router.get('/students-for-day', (req, res)=>{
   const sql = `SELECT 
   g.guest_id,
