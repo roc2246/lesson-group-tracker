2d5f25ee5ebf45ffb8c62a7f9c76f9e9c78f949d
Riley <riley.childs@yahoo.com>
2025-04-26 15:26:02 -0400

    Imported environmental valriables to db code

--- db/index.js
+++ db/index.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql");
+require("dotenv").config({ path: './config/.env' });
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
--- db/index.test.js
+++ db/index.test.js
@@ -1,11 +1,23 @@
 import { describe, it, expect, vi, beforeEach } from "vitest";
 import { dbLogin, handleQuery } from "../db/index";
-import { mockConnSuccess, mockConnFail, mockQuery, mockQueryError } from "../__mocks__/db/index"; 
+import {
+  mockConnSuccess,
+  mockConnFail,
+  mockQuery,
+  mockQueryError,
+} from "../__mocks__/db/index";
 
 beforeEach(() => {
   vi.clearAllMocks();
 });
 
+// describe('Database interaction', () => { 
+//   it("should run dbLogin with regular connection", async () => {
+//     const message = await dbLogin();
+//     expect(message).toBe("Connected to the database!");
+
+//   });
+//  })
 
 describe("dbLogin", () => {
   it("should return mock connection message", async () => {
@@ -19,19 +31,21 @@ describe("dbLogin", () => {
   });
 });
 
-describe("handleQuery", ()=>{
-  it("should return data", async()=>{
-    const sql = "Mock SQL"
-    const values = ["mock", "values"]
-    const mockResults = await handleQuery(sql, values, mockQuery) 
-    const output = {test: "DATA"}
-    expect(mockResults).toStrictEqual(output)
-  })
+describe("handleQuery", () => {
+  it("should return data", async () => {
+    const sql = "Mock SQL";
+    const values = ["mock", "values"];
+    const mockResults = await handleQuery(sql, values, mockQuery);
+    const output = { test: "DATA" };
+    expect(mockResults).toStrictEqual(output);
+  });
 
   it("should reject on query error", async () => {
     const sql = "Bad SQL";
     const values = ["mock", "values"];
 
-    await expect(handleQuery(sql, values, mockQueryError)).rejects.toBe("Query error: SOME ERROR");
+    await expect(handleQuery(sql, values, mockQueryError)).rejects.toBe(
+      "Query error: SOME ERROR"
+    );
   });
-})
+});
